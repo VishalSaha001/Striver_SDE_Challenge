3579d1543792940bcfc16d332ce016783f5b25b2
Vs <sahavishal001@gmail.com>
2023-06-05 23:55:40 +0530

    kthLargestElementInStream

---
+++ kthLargestElementInStream.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+class Kthlargest
+{
+private:
+    priority_queue<int, vector<int>, greater<int>> pq;
+    int k;
+
+public:
+    Kthlargest(int k, vector<int> &arr)
+    {
+        // Write your code here.
+        this->k = k;
+        pq = priority_queue<int, vector<int>, greater<int>>();
+
+        for (int x : arr)
+        {
+            pq.push(x);
+            if (pq.size() > k)
+                pq.pop();
+        }
+    }
+
+    void add(int num)
+    {
+        // Write your code here.
+        pq.push(num);
+        if (pq.size() > k)
+            pq.pop();
+    }
+
+    int getKthLargest()
+    {
+        // Write your code here.
+        return pq.top();
+    }
+};
