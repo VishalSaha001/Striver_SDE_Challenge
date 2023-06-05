f16669ce6e55033212565e279e8425b3be9940a7
Vs <sahavishal001@gmail.com>
2023-06-05 15:54:40 +0530

    SymmetricTree

---
+++ SymmetricTree.cpp
@@ -0,0 +1,47 @@
+/*****************************************************
+
+    Following is the Binary Tree node structure:
+
+    class BinaryTreeNode {
+        public :
+        T data;
+        BinaryTreeNode<T> *left;
+        BinaryTreeNode<T> *right;
+
+        BinaryTreeNode(T data) {
+            this -> data = data;
+            left = NULL;
+            right = NULL;
+        }
+
+        ~BinaryTreeNode() {
+            if(left)
+                delete left;
+            if(right)
+                delete right;
+        }
+    };
+
+******************************************************/
+
+bool recur(BinaryTreeNode<int> *left, BinaryTreeNode<int> *right)
+{
+    if (left == NULL && right == NULL)
+        return true;
+
+    if (left == NULL || right == NULL)
+        return false;
+
+    if (left->data != right->data)
+        return false;
+
+    return recur(left->left, right->right) && recur(left->right, right->left);
+}
+
+bool isSymmetric(BinaryTreeNode<int> *root)
+{
+    if (root == NULL)
+        return true;
+
+    return recur(root->left, root->right);
+}
