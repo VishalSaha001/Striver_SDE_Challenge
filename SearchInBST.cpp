d52ea61259dd7773eebae5a4201fef40561d2465
Vs <sahavishal001@gmail.com>
2023-06-05 15:56:40 +0530

    SearchInBST

---
+++ SearchInBST.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+/*
+    Following is the Binary Tree node structure:
+
+    class BinaryTreeNode {
+    public :
+        T data;
+        BinaryTreeNode<T> *left;
+        BinaryTreeNode<T> *right;
+
+        BinaryTreeNode(T data) {
+            this -> data = data;
+            left = NULL;
+            right = NULL;
+        }
+    };
+*/
+
+bool searchInBST(BinaryTreeNode<int> *root, int x)
+{
+    // Write your code here.
+    while (root)
+    {
+        if (root->data == x)
+            return true;
+
+        if (x > root->data)
+            root = root->right;
+        else
+            root = root->left;
+    }
+    return false;
+}
